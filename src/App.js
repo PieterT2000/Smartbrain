c8bed6517d49fd4b5247e37f08fa81c49c924f33
PieterT2000 <pietervantuijl2000@gmail.com>
2019-03-01 15:33:26 +0100

    Small changes

--- src/App.js
+++ src/App.js
@@ -19,6 +19,20 @@ const particlesOptions = {
 };
 
 class App extends Component {
+	constructor() {
+		super();
+		this.state = {
+			input: ''
+		};
+	}
+
+	onInputChange = (event) => {
+		console.log(event.target.value);
+	};
+
+	onSubmit = () => {
+		console.log('click');
+	};
 	render() {
 		return (
 			<div className="App">
@@ -26,7 +40,7 @@ class App extends Component {
 				<Navigation />
 				<Logo />
 				<Rank />
-				<ImageLinkForm />
+				<ImageLinkForm onInputChange={this.onInputChange} onSubmit={this.onSubmit} />
 				{/* <FaceRecognition /> */}
 			</div>
 		);
