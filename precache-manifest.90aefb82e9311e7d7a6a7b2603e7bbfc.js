ac8b733661bc8750bdd33e2a3e5ffc3281b0cb12
PieterT2000 <pietervantuijl2000@gmail.com>
2020-03-05 18:33:59 +0100

    Updates

---
+++ precache-manifest.90aefb82e9311e7d7a6a7b2603e7bbfc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "175651bbea28cf89fe7f",
+    "url": "/Smartbrain/static/js/runtime~main.175651bb.js"
+  },
+  {
+    "revision": "b51e3d9b1b3a4a267bf8",
+    "url": "/Smartbrain/static/js/main.b51e3d9b.chunk.js"
+  },
+  {
+    "revision": "f08b9fc1391dce728493",
+    "url": "/Smartbrain/static/js/2.f08b9fc1.chunk.js"
+  },
+  {
+    "revision": "b51e3d9b1b3a4a267bf8",
+    "url": "/Smartbrain/static/css/main.3d4807ad.chunk.css"
+  },
+  {
+    "revision": "f08b9fc1391dce728493",
+    "url": "/Smartbrain/static/css/2.d7290d2d.chunk.css"
+  },
+  {
+    "revision": "49beba3a6f8377fa27321c8803babfbc",
+    "url": "/Smartbrain/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/smartbrain/precache-manifest.75c8d17b0ea74f90b15290351a18e903.js"
+  "/Smartbrain/precache-manifest.90aefb82e9311e7d7a6a7b2603e7bbfc.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/smartbrain/index.html", {
+workbox.routing.registerNavigationRoute("/Smartbrain/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
